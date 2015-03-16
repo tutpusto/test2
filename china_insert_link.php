26b2256be0d1c0c6530a1b1e45b0d8dbdaea5d8d
tutpusto <nanoscale@yandex.ru>
2015-03-16 17:31:51 +0300

    check links

--- china_insert_link.php
+++ china_insert_link.php
@@ -16,10 +16,18 @@ $query="select id from teachers where name='".$teacher."'";
 $result=mysql_query($query);
 $teacher_id=mysql_result($result,0,"id");
 
+$query="select id from links where teacher=".$teacher_id." and pupil=".$pupil_id;
+$result=mysql_query($query);
+$rows=mysql_num_rows($result);
+if ($rows==0) {
 $query="insert into links values(null,".$pupil_id.",".$teacher_id.")";
 $result=mysql_query($query) or die(mysql_error());
 //<!-- If is set URL variables and insert is ok, show the site name -->
 echo "Cвязь добавлена.";
+}
+else {
+echo "Такая связь уже есть";
+}
 
 
 ?>
